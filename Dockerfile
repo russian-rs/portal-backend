f59a1ea6353510278544d52b069c837e196f1945
Max Aminov <aminovmaksim@gmail.com>
2024-08-16 12:51:09 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# Use the official Gradle image with JDK 21 installed
+FROM gradle:8.10.0-jdk21-alpine AS build
+
+# Set work directory
+WORKDIR /home/gradle/src
+
+# Copy your source code into the container
+COPY . /home/gradle/src
+
+# Build the project using Gradle
+RUN gradle clean build --no-daemon
+
+# For the runtime stage use the official OpenJDK 21 image from Docker Hub
+FROM alpine/java:21.0.4-jdk
+
+# Expose port 8080 for your application
+EXPOSE 8080
+
+# Set work directory
+WORKDIR /app
+
+# Copy the built jar file from the build stage into this new container
+COPY --from=build /home/gradle/src/build/libs/*.jar ./app.jar
+
+ENTRYPOINT exec java -jar -Dspring.profiles.active=$ACTIVE_PROFILES ./app.jar
---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	kotlin("jvm") version "1.9.24"
+	kotlin("plugin.spring") version "1.9.24"
+	id("org.springframework.boot") version "3.3.2"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "rs.russian"
+version = "1.0.0"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.getByName<Jar>("jar") {
+	enabled = false
+}
