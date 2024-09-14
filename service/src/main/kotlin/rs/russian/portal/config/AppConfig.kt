5dca0fec21e22655dbe83ff9bc9870f6796a24b5
Max Aminov <aminovmaksim@gmail.com>
2024-09-14 22:06:32 +0200

    oauth via authentik

--- service/src/main/kotlin/rs/russian/portal/config/AppConfig.kt
+++ service/src/main/kotlin/rs/russian/portal/config/AppConfig.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import net.javacrumbs.shedlock.core.LockProvider
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.core.JdbcTemplate
@@ -13,6 +14,7 @@ import javax.sql.DataSource
 
 @Configuration
 @EnableScheduling
+@EnableConfigurationProperties(value = [AppProperties::class])
 @EnableSchedulerLock(defaultLockAtMostFor = "PT59S")
 class AppConfig {
 
