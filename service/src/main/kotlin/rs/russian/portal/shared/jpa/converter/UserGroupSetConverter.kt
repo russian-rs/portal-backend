0cc2f075e8fa18df19602b49dc15de6a371f28df
Max Aminov <aminovmaksim@gmail.com>
2024-12-01 00:49:36 +0100

    feature: user groups

---
+++ service/src/main/kotlin/rs/russian/portal/shared/jpa/converter/UserGroupSetConverter.kt
@@ -0,0 +1,29 @@
+package rs.russian.portal.shared.jpa.converter
+
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.persistence.AttributeConverter
+import jakarta.persistence.Converter
+import rs.russian.portal.shared.enums.UserGroup
+
+@Converter
+class UserGroupSetConverter(
+    private val objectMapper: ObjectMapper
+) : AttributeConverter<Set<UserGroup>, String> {
+
+    override fun convertToDatabaseColumn(attribute: Set<UserGroup>): String {
+        return objectMapper.writeValueAsString(attribute)
+    }
+
+    override fun convertToEntityAttribute(value: String): Set<UserGroup> {
+        return try {
+            val groups = mutableSetOf<UserGroup>()
+            objectMapper.readValue(value, object : TypeReference<Set<String>>() {}).forEach { groupName ->
+                UserGroup.safeValueOf(groupName)?.let { groups.add(it) }
+            }
+            return groups
+        } catch (ex: Exception) {
+            mutableSetOf()
+        }
+    }
+}
--- service/src/main/kotlin/rs/russian/portal/user/mapper/UserMapper.kt
+++ service/src/main/kotlin/rs/russian/portal/user/mapper/UserMapper.kt
@@ -4,11 +4,14 @@ import org.mapstruct.Mapper
 import org.mapstruct.Mapping
 import org.mapstruct.MappingConstants.ComponentModel.SPRING
 import org.mapstruct.MappingTarget
+import org.mapstruct.Named
 import org.mapstruct.ReportingPolicy.ERROR
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo
 import rs.russian.generated.model.UserInfoDto
 import rs.russian.portal.file.mapper.FileInfoMapper
+import rs.russian.portal.shared.enums.UserGroup
+import rs.russian.portal.shared.security.userGroups
 import rs.russian.portal.user.domain.Account
 import rs.russian.portal.user.domain.UserInfo
 
@@ -28,6 +31,8 @@ abstract class UserMapper {
     @Mapping(target = "version", ignore = true)
     @Mapping(target = "reports", expression = "java(new ArrayList<>())")
     @Mapping(target = "username", source = "nickName")
+    @Mapping(target = "groups", source = "oidcUserInfo", qualifiedByName = ["mapGroups"])
+    @Mapping(target = "disabled", constant = "false")
     abstract fun map(oidcUserInfo: OidcUserInfo): Account
 
     @Mapping(target = "id", ignore = true)
@@ -35,6 +40,8 @@ abstract class UserMapper {
     @Mapping(target = "version", ignore = true)
     @Mapping(target = "reports", ignore = true)
     @Mapping(target = "username", source = "nickName")
+    @Mapping(target = "groups", source = "oidcUserInfo", qualifiedByName = ["mapGroups"])
+    @Mapping(target = "disabled", ignore = true)
     abstract fun update(oidcUserInfo: OidcUserInfo, @MappingTarget account: Account)
 
     @Mapping(target = "userId", source = "id")
@@ -43,4 +50,8 @@ abstract class UserMapper {
     @Mapping(target = "fullName", source = "account.fullName")
     abstract fun map(userInfo: UserInfo?): UserInfoDto
 
+    @Named("mapGroups")
+    fun mapGroups(oidcUserInfo: OidcUserInfo): Set<UserGroup> {
+        return oidcUserInfo.userGroups()
+    }
 }
