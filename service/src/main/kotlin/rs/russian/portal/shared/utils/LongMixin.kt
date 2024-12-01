68c571e198923fe10128f308501afd2e333b95b6
Max Aminov <aminovmaksim@gmail.com>
2024-12-01 17:58:21 +0100

    bugfix: long jackson mixin

--- service/src/main/kotlin/rs/russian/portal/shared/utils/LongMixin.kt
+++ service/src/main/kotlin/rs/russian/portal/shared/utils/LongMixin.kt
@@ -1,6 +1,3 @@
 package rs.russian.portal.shared.utils
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 abstract class LongMixin
