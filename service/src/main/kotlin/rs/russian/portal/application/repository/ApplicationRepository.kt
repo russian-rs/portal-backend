55214f1e5440cd5c449c63e1646442f28ee71c37
Max Aminov <aminovmaksim@gmail.com>
2024-12-20 18:17:54 +0100

    feature: application

--- service/src/main/kotlin/rs/russian/portal/application/repository/ApplicationRepository.kt
+++ service/src/main/kotlin/rs/russian/portal/application/repository/ApplicationRepository.kt
@@ -6,12 +6,15 @@ import org.springframework.data.jpa.domain.Specification
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import rs.russian.portal.application.domain.Application
+import rs.russian.portal.application.domain.ApplicationStatus
 import java.util.*
 
 @Repository
 interface ApplicationRepository : JpaRepository<Application, UUID> {
 
     fun findByEmail(email: String): Optional<Application>
+
+    fun findByEmailAndStatusNotIn(email: String, statuses: List<ApplicationStatus>): Optional<Application>
 
     fun findByEmailAndPassport(email: String, passport: String): Optional<Application>
 
