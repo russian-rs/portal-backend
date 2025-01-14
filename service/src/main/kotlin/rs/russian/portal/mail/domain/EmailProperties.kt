ebd559476c82f9b6a4d148169860ba9b703e9564
Max Aminov <aminovmaksim@gmail.com>
2025-01-14 18:36:35 +0100

    feature: email api

--- service/src/main/kotlin/rs/russian/portal/mail/domain/EmailProperties.kt
+++ service/src/main/kotlin/rs/russian/portal/mail/domain/EmailProperties.kt
@@ -2,7 +2,7 @@ package rs.russian.portal.mail.domain
 
 data class EmailProperties(
     var toList: List<String>,
-    var ccList: List<String> = mutableListOf(),
+    var ccList: List<String> = mutableListOf("Портал Волонтера <portal@russian.rs>"),
     var subject: String,
     var body: String,
     var from: String = "Русская Диаспора <info@russian.rs>"
