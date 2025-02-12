4ec9d1c14250409fb0d0d1705c68519b6c4da353
Max Aminov <aminovmaksim@gmail.com>
2025-02-12 18:13:22 +0100

    feature: user search request

--- service/src/main/kotlin/rs/russian/portal/user/domain/specification/AccountSpecification.kt
+++ service/src/main/kotlin/rs/russian/portal/user/domain/specification/AccountSpecification.kt
@@ -10,22 +10,22 @@ import rs.russian.portal.user.domain.Account_
 import rs.russian.portal.user.domain.UserInfo_
 
 fun searchSpecification(query: String, filter: UserSearchFilter?): Specification<Account> {
-    if (query.isEmpty()) {
-        return empty()
-    }
-
     var resultSpec: Specification<Account> = empty()
 
-    val querySpec = like<Account>(Account_.FULL_NAME, query)
-        .or(like(Account_.USERNAME, query))
-        .or(like(Account_.EMAIL, query))
-        .or(like(Account_.INFO, UserInfo_.TELEGRAM, query))
-        .or(like(Account_.INFO, UserInfo_.PHONE, query))
-    resultSpec = resultSpec.and(querySpec)
+    if (query.isNotBlank()) {
+        val querySpec = like<Account>(Account_.FULL_NAME, query)
+            .or(like(Account_.USERNAME, query))
+            .or(like(Account_.EMAIL, query))
+            .or(like(Account_.INFO, UserInfo_.TELEGRAM, query))
+            .or(like(Account_.INFO, UserInfo_.PHONE, query))
+        resultSpec = resultSpec.and(querySpec)
+    }
 
     filter?.let {
         var filterSpec: Specification<Account> = empty()
-        filterSpec = filterSpec.and(equal(Account_.ACTIVE, filter.active))
+
+        if (filter.onlyInactive)
+            filterSpec = filterSpec.and(equal(Account_.ACTIVE, false))
 
         resultSpec = resultSpec.and(filterSpec)
     }
