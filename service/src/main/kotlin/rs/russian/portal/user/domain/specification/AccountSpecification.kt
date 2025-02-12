794eea34d80920fe952e36fb773e379c123bb612
Max Aminov <aminovmaksim@gmail.com>
2025-02-12 17:21:13 +0100

    feature: new roles

--- service/src/main/kotlin/rs/russian/portal/user/domain/specification/AccountSpecification.kt
+++ service/src/main/kotlin/rs/russian/portal/user/domain/specification/AccountSpecification.kt
@@ -1,19 +1,34 @@
 package rs.russian.portal.user.domain.specification
 
 import org.springframework.data.jpa.domain.Specification
+import rs.russian.generated.model.UserSearchFilter
 import rs.russian.portal.shared.jpa.empty
+import rs.russian.portal.shared.jpa.equal
 import rs.russian.portal.shared.jpa.like
 import rs.russian.portal.user.domain.Account
 import rs.russian.portal.user.domain.Account_
 import rs.russian.portal.user.domain.UserInfo_
 
-fun searchSpecification(query: String): Specification<Account> {
+fun searchSpecification(query: String, filter: UserSearchFilter?): Specification<Account> {
     if (query.isEmpty()) {
         return empty()
     }
-    return like<Account>(Account_.FULL_NAME, query)
+
+    var resultSpec: Specification<Account> = empty()
+
+    val querySpec = like<Account>(Account_.FULL_NAME, query)
         .or(like(Account_.USERNAME, query))
         .or(like(Account_.EMAIL, query))
         .or(like(Account_.INFO, UserInfo_.TELEGRAM, query))
         .or(like(Account_.INFO, UserInfo_.PHONE, query))
+    resultSpec = resultSpec.and(querySpec)
+
+    filter?.let {
+        var filterSpec: Specification<Account> = empty()
+        filterSpec = filterSpec.and(equal(Account_.ACTIVE, filter.active))
+
+        resultSpec = resultSpec.and(filterSpec)
+    }
+
+    return resultSpec
 }
