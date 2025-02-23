a97918878f49ba315f3e1e0a77c9e4ff09a3d6e2
Max Aminov <aminovmaksim@gmail.com>
2025-02-23 16:04:34 +0100

    feature: application contract date

--- service/src/main/kotlin/rs/russian/portal/application/service/ApplicationEventListener.kt
+++ service/src/main/kotlin/rs/russian/portal/application/service/ApplicationEventListener.kt
@@ -6,17 +6,21 @@ import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.event.TransactionalEventListener
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.context.Context
+import rs.russian.generated.model.ContractDto
 import rs.russian.portal.application.domain.ApplicationStatus
 import rs.russian.portal.application.domain.ApplicationType
 import rs.russian.portal.application.event.ApplicationCreatedEvent
 import rs.russian.portal.application.event.ApplicationUpdateEvent
 import rs.russian.portal.mail.service.EmailService
+import rs.russian.portal.user.mapper.ContractMapper
 import rs.russian.portal.user.service.AccountService
+import java.util.*
 
 @Component
 class ApplicationEventListener(
     private val emailService: EmailService,
     private val accountService: AccountService,
+    private val contractMapper: ContractMapper,
     private val templateEngine: TemplateEngine,
     private val applicationService: ApplicationService
 ) {
@@ -39,9 +43,36 @@ class ApplicationEventListener(
     @TransactionalEventListener(fallbackExecution = true)
     fun handleApplicationStatusChange(event: ApplicationUpdateEvent) {
         val application = applicationService.get(event.id)
-        if (application.status == ApplicationStatus.DONE && application.type == ApplicationType.NEW) {
-            if (accountService.findAccountByEmail(application.email) == null) {
-                accountService.create(application.email, application.name)
+        if (application.status == ApplicationStatus.DONE) {
+            if (application.type == ApplicationType.NEW) {
+                if (accountService.findAccountByEmail(application.email) == null) {
+                    val account = accountService.create(application.email, application.name)
+                    accountService.updateContracts(
+                        account,
+                        listOf(
+                            ContractDto(
+                                id = UUID.randomUUID(),
+                                startDate = application.contractFrom!!,
+                                endDate = application.contractUntil!!,
+                                type = application.contractType!!
+                            )
+                        )
+                    )
+                }
+            }
+            if (application.type == ApplicationType.PROLONGATION) {
+                val account = accountService.findAccountByEmail(application.email)!!
+                accountService.switchActiveState(account, true)
+                val contracts = contractMapper.map(account.contracts)
+                contracts.add(
+                    ContractDto(
+                        id = UUID.randomUUID(),
+                        startDate = application.contractFrom!!,
+                        endDate = application.contractUntil!!,
+                        type = application.contractType!!
+                    )
+                )
+                accountService.updateContracts(account, contracts)
             }
         }
     }
