82b2b119893556549d06000f232397ec17f1c36a
Alexey <arsennikum@gmail.com>
2025-04-21 18:40:09 +0300

    multi wordpress: fix apiClient init

--- service/src/main/kotlin/rs/russian/portal/user/service/wordpress/WordpressUserService.kt
+++ service/src/main/kotlin/rs/russian/portal/user/service/wordpress/WordpressUserService.kt
@@ -1,8 +1,10 @@
 package rs.russian.portal.user.service.wordpress
 
+import org.wordpress.api.UsersWordpressApi
 import org.wordpress.model.WpUser
 
 interface WordpressUserService {
+    var apiClient: UsersWordpressApi
 
     fun getUser(username: String): WpUser?
 
