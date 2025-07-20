999fbd4d7027ad2ea2ceab14a196251a50143c73
ValeriiLindenPy <v.linden@mail.ru>
2025-07-20 15:33:37 +0200

    fix: unused imports

--- service/src/main/kotlin/rs/russian/portal/report/service/ReportService.kt
+++ service/src/main/kotlin/rs/russian/portal/report/service/ReportService.kt
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.bind.annotation.PathVariable
 import rs.russian.generated.model.NoteDto
 import rs.russian.generated.model.ReportDto
 import rs.russian.generated.model.ReportFilter
