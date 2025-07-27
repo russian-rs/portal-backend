845a659e49bf2baa6c23a2c53a3773285d8d96fd
ValeriiLindenPy <v.linden@mail.ru>
2025-07-27 12:53:50 +0200

    feat: add StatisticGroup to a Project, add liquibase changelog for it. Add field to openapi. Add gender to update user info

---
+++ service/src/main/kotlin/rs/russian/portal/program/domain/StatisticGroup.kt
@@ -0,0 +1,9 @@
+package rs.russian.portal.program.domain
+
+enum class StatisticGroup {
+    MEDIJI_I_KOMUNIKACIJE,
+    SOCIJALNA_ZASTITA,
+    KULTURNA_DOBA,
+    ZIVOTNA_SREDINA,
+    JAVNE_POVRSINE
+}
--- service/src/main/kotlin/rs/russian/portal/user/repository/AccountRepository.kt
+++ service/src/main/kotlin/rs/russian/portal/user/repository/AccountRepository.kt
@@ -5,9 +5,12 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.domain.Specification
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import rs.russian.portal.user.domain.Account
 import rs.russian.portal.user.domain.Account.Companion.GRAPH_FULL
+import rs.russian.portal.user.repository.projections.AgeSliceCountProjection
+import rs.russian.portal.user.repository.projections.GenderCountProjection
 import java.util.*
 
 @Repository
@@ -30,4 +33,32 @@ interface AccountRepository : JpaRepository<Account, Int> {
 
     @EntityGraph(value = GRAPH_FULL)
     fun findAll(specification: Specification<Account>): List<Account>
+
+    @Query(
+        """
+    SELECT ui.gender AS gender, COUNT(ui) AS count
+    FROM UserInfo ui
+    GROUP BY ui.gender
+"""
+    )
+    fun countByGender(): List<GenderCountProjection>
+
+    @Query(
+        """
+    SELECT
+        COUNT(CASE WHEN age BETWEEN 15 AND 18 THEN 1 END) AS age15to18Count,
+        COUNT(CASE WHEN age > 18 AND age <= 30 THEN 1 END) AS age18to30Count,
+        COUNT(CASE WHEN age > 30 AND age <= 40 THEN 1 END) AS age30to40Count,
+        COUNT(CASE WHEN age > 40 AND age <= 65 THEN 1 END) AS age40to65Count,
+        COUNT(CASE WHEN age > 65 THEN 1 END) AS age65AndAboveCount
+    FROM (
+        SELECT EXTRACT(YEAR FROM AGE(CURRENT_DATE, ui.birth_date)) AS age
+        FROM user_info ui
+        WHERE ui.birth_date IS NOT NULL
+    ) AS derived
+    """,
+        nativeQuery = true
+    )
+    fun countByAgeSlices(): AgeSliceCountProjection
+
 }
