eb692a8c572f4ac8c46cbab663a041b4a659b967
Max Aminov <aminovmaksim@gmail.com>
2026-02-10 20:38:10 +0100

    bugfix: wordpress user sync fix

--- service/src/test/kotlin/rs/russian/portal/testconfig/AbstractIntegrationTest.kt
+++ service/src/test/kotlin/rs/russian/portal/testconfig/AbstractIntegrationTest.kt
@@ -1,14 +1,11 @@
 package rs.russian.portal.testconfig
 
-import io.github.serpro69.kfaker.provider.Internet
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.junit.jupiter.Testcontainers
-import kotlin.properties.Delegates
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
