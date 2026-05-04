127cc370d1dcceffed9c4fc96bd145d1cb40d3d5
ValeriiLindenPy <valeriilinden@gmail.com>
2026-05-04 15:17:26 +0200

    feat: add aoi for official groups

--- service/src/main/kotlin/rs/russian/portal/program/mapper/ProgramMapper.kt
+++ service/src/main/kotlin/rs/russian/portal/program/mapper/ProgramMapper.kt
@@ -2,7 +2,9 @@ package rs.russian.portal.program.mapper
 
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import org.mapstruct.Named
 import rs.russian.generated.model.ProgramDto
+import rs.russian.portal.program.domain.OfficialGroup
 import rs.russian.portal.program.domain.Program
 import rs.russian.portal.program.domain.Project
 
@@ -10,9 +12,15 @@ import rs.russian.portal.program.domain.Project
 @Mapper
 abstract class ProgramMapper {
 
-    @Mapping(target = "projectCodes", source = "projects")
+    @Mapping(target = "projectCodes", source = "projects", qualifiedByName = ["projectCodes"])
+    @Mapping(target = "officialGroups", source = "officialGroups", qualifiedByName = ["officialGroupCodes"])
     abstract fun toDto(program: Program?): ProgramDto
 
-    protected fun map(projects: Set<Project>?): List<String> =
+    @Named("projectCodes")
+    protected fun mapProjects(projects: Set<Project>?): List<String> =
         projects?.map { it.code } ?: emptyList()
+
+    @Named("officialGroupCodes")
+    protected fun mapOfficialGroups(officialGroups: Set<OfficialGroup>?): List<String> =
+        officialGroups?.map { it.code } ?: emptyList()
 }
--- service/src/main/kotlin/rs/russian/portal/shared/utils/CacheService.kt
+++ service/src/main/kotlin/rs/russian/portal/shared/utils/CacheService.kt
@@ -18,12 +18,14 @@ class CacheService {
         const val PROGRAM_DICT_CACHE_NAME = "program_dict_cache"
         const val PROJECT_DICT_CACHE_NAME = "project_dict_cache"
         const val CITIES_CACHE_NAME = "cities_cache"
+        const val OFFICIAL_GROUP_DICT_CACHE_NAME = "official_group_dict_cache"
 
         val CACHE_MAP = mapOf<String, Cache<Any, Any>>(
             S3_FILE_CACHE_NAME to Caffeine.newBuilder().expireAfterWrite(12, HOURS).build(),
             PROGRAM_DICT_CACHE_NAME to Caffeine.newBuilder().expireAfterWrite(1, HOURS).build(),
             PROJECT_DICT_CACHE_NAME to Caffeine.newBuilder().expireAfterWrite(1, HOURS).build(),
             CITIES_CACHE_NAME to Caffeine.newBuilder().expireAfterWrite(1, HOURS).build(),
+            OFFICIAL_GROUP_DICT_CACHE_NAME to Caffeine.newBuilder().expireAfterWrite(1, HOURS).build(),
         )
     }
 }
